d971e3243c6880272875c40ead10c49cc6bd904e
linjun <1045735402@qq.com>
2019-03-19 11:16:10 +0800

    Refactor test case

--- test/emqx_dashboard_SUITE.erl
+++ test/emqx_dashboard_SUITE.erl
@@ -14,117 +14,30 @@
 
 -define(BASE_PATH, "api").
 
+-define(OVERVIEWS, [alarms, banned, brokers, stats, metrics, listeners, connections, sessions, subscriptions, routes, plugins]).
+
 all() -> 
     [{group, overview},
-     %{group, connections},
-     %{group, sessions},
-     %{group, routes},
-     %{group, subscriptions},
      {group, admins},
      {group, cli}
      ].
 
 groups() ->
-    [{overview, [sequence], [brokers, stats,
-                             nodes, metrics, listeners]},
-     {connections, [sequence], [connections, clients_query]},
-     {sessions, [sequence], [session_query]},
-     {routes, [sequence], [route_query]},
-     {subscriptions, [sequence], [subscribe_query]},
+    [{overview, [sequence], [overview]},
      {admins, [sequence], [admins_add_delete]},
      {cli, [sequence], [cli]}
     ].
 
 init_per_suite(Config) ->
-    [start_apps(App, {SchemaFile, ConfigFile}) ||
-        {App, SchemaFile, ConfigFile}
-            <- [{emqx, local_path("deps/emqx/priv/emqx.schema"),
-                       local_path("deps/emqx/etc/emqx.conf")},
-                {emqx_management, local_path("deps/emqx_management/priv/emqx_management.schema"),
-                                  local_path("deps/emqx_management/etc/emqx_management.conf")},
-                {emqx_dashboard, local_path("priv/emqx_dashboard.schema"),
-                                 local_path("etc/emqx_dashboard.conf")}]],
+    emqx_ct_helpers:start_apps([emqx, emqx_management, emqx_dashboard]),
     Config.
 
 end_per_suite(_Config) ->
-    application:stop(emqx_dashboard),
-    application:stop(emqx),
+    emqx_ct_helpers:stop_apps([emqx, emqx_management, emqx_dashboard]),
     ekka_mnesia:ensure_stopped().
 
-get_base_dir() ->
-    {file, Here} = code:is_loaded(?MODULE),
-    filename:dirname(filename:dirname(Here)).
-
-local_path(RelativePath) ->
-    filename:join([get_base_dir(), RelativePath]).
-
-start_apps(App, {SchemaFile, ConfigFile}) ->
-    read_schema_configs(App, {SchemaFile, ConfigFile}),
-    set_special_configs(App),
-    application:ensure_all_started(App).
-
-read_schema_configs(App, {SchemaFile, ConfigFile}) ->
-    ct:pal("Read configs - SchemaFile: ~p, ConfigFile: ~p", [SchemaFile, ConfigFile]),
-    Schema = cuttlefish_schema:files([SchemaFile]),
-    Conf = conf_parse:file(ConfigFile),
-    NewConfig = cuttlefish_generator:map(Schema, Conf),
-    Vals = proplists:get_value(App, NewConfig, []),
-    [application:set_env(App, Par, Value) || {Par, Value} <- Vals].
-
-set_special_configs(emqx) ->
-    application:set_env(emqx, plugins_loaded_file,
-                        local_path("deps/emqx/test/emqx_SUITE_data/loaded_plugins"));
-set_special_configs(_App) ->
-    ok.
-
-brokers(_) ->
-    ?assert(request_dashbaord(get, api_path("brokers"), auth_header_())).
-
-stats(_) ->
-    ?assert(request_dashbaord(get, api_path("stats"), auth_header_())).
-
-nodes(_) ->
-    ?assert(request_dashbaord(get, api_path("nodes"), auth_header_())).
-
-metrics(_) ->
-    ?assert(request_dashbaord(get, api_path("metrics"), auth_header_())).
-
-listeners(_) ->
-    ?assert(request_dashbaord(get, api_path("listeners"), auth_header_())).
-
-connections(_) ->
-    ?assert(request_dashbaord(get, api_path("connections"), "page_size=100&curr_page=1", auth_header_())).
-
-clients_query(_) ->
-    Sock = client_connect_(<<16,12,0,4,77,81,84,84,4,0,0,90,0,0>>, 4),
-    {ok, Entry} = emqx_dashboard_client:list(<<>>, 1, 100),
-    Client = proplists:get_value(result, Entry),
-    ClientId = proplists:get_value(clientId, Client), 
-    ?assertEqual({ok, Entry}, emqx_dashboard_client:list(ClientId, 1, 100)),
-    gen_tcp:close(Sock).
-
-session_query(_) ->
-    Sock = client_connect_(<<16,12,0,4,77,81,84,84,4,0,0,90,0,0>>, 4),
-    {ok, Entry} = emqx_dashboard_session:list(<<>>, 1, 100),
-    Session= proplists:get_value(result, Entry),
-    ClientId = proplists:get_value(clientId, Session), 
-    ?assertEqual({ok, Entry}, emqx_dashboard_session:list(ClientId, 1, 100)),
-    gen_tcp:close(Sock).
-
-route_query(_) ->
-    ok = emqx:subscribe(<<"topic">>),
-    timer:sleep(10),
-    {ok, Routes} = emqx_dashboard_route:list(<<>>, 1, 100),
-    ?assertEqual({ok, Routes}, emqx_dashboard_route:list(<<"topic">>, 1, 100)),
-    ok = emqx:unsubscribe(<<"topic">>).
-
-subscribe_query(_) ->
-    Sock = client_connect_(<<16,12,0,4,77,81,84,84,4,0,0,90,0,0>>, 4),
-    {ok, Entry} = emqx_dashboard_subscription:list(<<>>, 1, 100),
-    Sub = proplists:get_value(result, Entry),
-    ClientId = proplists:get_value(clientId, Sub), 
-    ?assertEqual({ok, Entry}, emqx_dashboard_subscription:list(ClientId, 1, 100)),
-    gen_tcp:close(Sock).
+overview(_) ->
+    [?assert(request_dashbaord(get, api_path(erlang:atom_to_list(Overview)), auth_header_()))|| Overview <- ?OVERVIEWS].
 
 admins_add_delete(_) ->
     ok = emqx_dashboard_admin:add_user(<<"username">>, <<"password">>, <<"tag">>),
